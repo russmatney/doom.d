e574eab536919737a2e18462b890246e7e227f8b
Russell Matney <russell.matney@gmail.com>
2018-02-19 19:22:16 -0500

    fix: evil autoloads using dead dirs

--- autoload/evil.el
+++ autoload/evil.el
@@ -1,26 +1,26 @@
 ;;; private/russ/autoload/evil.el -*- lexical-binding: t; -*-
 
-;;;###autoload (autoload '+russ:multi-next-line "private/russ/autoload/evil" nil t)
+;;;###autoload
 (evil-define-motion +russ:multi-next-line (count)
   "Move down 6 lines."
   :type line
   (let ((line-move-visual (or visual-line-mode (derived-mode-p 'text-mode))))
     (evil-line-move (* 6 (or count 1)))))
 
-;;;###autoload (autoload '+russ:multi-previous-line "private/russ/autoload/evil" nil t)
+;;;###autoload
 (evil-define-motion +russ:multi-previous-line (count)
   "Move up 6 lines."
   :type line
   (let ((line-move-visual (or visual-line-mode (derived-mode-p 'text-mode))))
     (evil-line-move (- (* 6 (or count 1))))))
 
-;;;###autoload (autoload '+russ:cd "private/russ/autoload/evil" nil t)
+;;;###autoload
 (evil-define-command +russ:cd ()
   "Change `default-directory' with `cd'."
   (interactive "<f>")
   (cd input))
 
-;;;###autoload (autoload '+russ:kill-all-buffers "private/russ/autoload/evil" nil t)
+;;;###autoload
 (evil-define-command +russ:kill-all-buffers (&optional bang)
   "Kill all buffers. If BANG, kill current session too."
   (interactive "<!>")
@@ -28,7 +28,7 @@
       (+workspace/kill-session)
     (doom/kill-all-buffers)))
 
-;;;###autoload (autoload '+russ:kill-matching-buffers "private/russ/autoload/evil" nil t)
+;;;###autoload
 (evil-define-command +russ:kill-matching-buffers (&optional bang pattern)
   "Kill all buffers matching PATTERN regexp. If BANG, only match project
 buffers."
